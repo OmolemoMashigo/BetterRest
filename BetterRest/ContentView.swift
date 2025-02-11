c3faa1e0699eec4aee918f58814883e0bc69e86d
Omolemo Mashigo <omomashigo@gmail.com>
2025-02-12 00:29:15 +0200

    working with different Date properties

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -13,6 +13,12 @@ struct ContentView: View {
     @State private var wakeUp = Date.now
     var body: some View {
         VStack{
+            Text(Date.now, format: .dateTime.hour().minute().second())
+            
+            Text(Date.now, format: .dateTime.day().month().year())
+            
+            Text(Date.now.formatted(date: .long, time: .shortened))
+            
             Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.5)
             .padding()
             
@@ -25,6 +31,14 @@ struct ContentView: View {
     func dateExamples(){
         let tomorrow = Date.now.addingTimeInterval(86400)
         let range = Date.now...tomorrow //date range from present time to tomorrow
+        
+        var components1 = DateComponents()
+        components1.hour = 8
+        components1.minute = 0
+        
+        let components2 = Calendar.current.dateComponents([.hour, .minute], from: .now)
+        let hour = components2.hour ?? 0 //should there not be an hour returned
+        let minute = components2.minute ?? 0 //should there not be a minute returned
     }
 }
 
