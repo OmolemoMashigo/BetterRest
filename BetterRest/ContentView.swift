d3ed1b306730046a964bc18dde18793c5e35572f
Omolemo Mashigo <omomashigo@gmail.com>
2025-02-13 18:25:28 +0200

    added DatePicker and Steppers for final implementation

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,38 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sleepAmount = 8.0
     
     @State private var wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+   
     var body: some View {
-        VStack{
-            Text(Date.now, format: .dateTime.hour().minute().second())
-            
-            Text(Date.now, format: .dateTime.day().month().year())
-            
-            Text(Date.now.formatted(date: .long, time: .shortened))
-            
-            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.5)
-            .padding()
-            
-            DatePicker("Enter a date", selection: $wakeUp, in: Date.now...)
+        NavigationStack{
+            VStack{
+                
+                Text("when do u wake up?")
+                    .font(.headline)
+                
+                DatePicker("Enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Text("Desired amount of sleep:")
+                
+                Stepper("\(sleepAmount.formatted())", value: $sleepAmount, in: 4...12, step: 0.5)
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                
+                Stepper("\(coffeeAmount) cup(s)", value: $coffeeAmount, in: 1...20)
+                .padding()
+                
+                
+            }
+            .navigationTitle("BetterRest")
+            .toolbar{
+                Button("Calculate", action: calculateBedTime)
+            }
         }
-        
         
     }
     
-    func dateExamples(){
-        let tomorrow = Date.now.addingTimeInterval(86400)
-        let range = Date.now...tomorrow //date range from present time to tomorrow
+    func calculateBedTime(){
         
-        var components1 = DateComponents()
-        components1.hour = 8
-        components1.minute = 0
-        
-        let components2 = Calendar.current.dateComponents([.hour, .minute], from: .now)
-        let hour = components2.hour ?? 0 //should there not be an hour returned
-        let minute = components2.minute ?? 0 //should there not be a minute returned
     }
+    
 }
 
 #Preview {
